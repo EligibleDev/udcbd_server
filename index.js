4b0573a4125e7c551e458292a76151549c5df5a0
EligibleDev <eligibledev@gmail.com>
2024-03-04 15:01:37 +0600

    links get api route created

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const port = process.env.PORT || 5000;
-
 const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json())
 
 const db = require('./models');
+
+// Routers
+const linkRouter = require("./routes/links");
+app.use("/api/v1/links", linkRouter);
 
 db.sequelize.sync().then(() => {
       app.listen(port, () => {
