ed38eb0b8f846dd5f8c661d6de8df18694c0ec3d
EligibleDev <eligibledev@gmail.com>
2024-03-06 15:28:38 +0600

    cors option added

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors")
+
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.json())
 
-const db = require('./models');
+app.use(express.json())
+app.use(cors({
+      origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "https://udcbd-bd.web.app",
+            "https://udcbd-bd.firebase.app",
+      ],
+}))
+
+const db = require("./models");
 
 // Routers
 const linkRouter = require("./routes/links");
